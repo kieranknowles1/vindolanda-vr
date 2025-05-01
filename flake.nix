d67819cf353caf8be9f39e928dc8fb11d8dc9ede
Kieran <kieranknowles11@hotmail.co.uk>
2025-05-01 11:59:07 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,83 @@
+{
+  description = "A flake based on my NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    systems.url = "github:nix-systems/default-linux";
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
+    nixcfg = {
+      url = "github:kieranknowles1/nixcfg";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-parts.follows = "flake-parts";
+      inputs.systems.follows = "systems";
+
+      # Disable inputs not needed outside of nixcfg
+      # These can be re-enabled if needed
+      inputs.clan-core.follows = "";
+      inputs.factorio-blueprints.follows = "";
+      inputs.firefox-addons.follows = "";
+      inputs.flake-utils-plus.follows = "";
+      inputs.flake-utils.follows = "";
+      inputs.home-manager.follows = "";
+      inputs.nix-index-database.follows = "";
+      inputs.nixos-cosmic.follows = "";
+      inputs.nixpkgs-openmw.follows = "";
+      inputs.nixpkgs-stable.follows = "";
+      inputs.openmw.follows = "";
+      inputs.snowfall-lib.follows = "";
+      inputs.sops-nix.follows = "";
+      inputs.src-factorio-blueprint-decoder.follows = "";
+      inputs.src-tldr.follows = "";
+      inputs.stylix.follows = "";
+      inputs.vscode-extensions.follows = "";
+    };
+  };
+
+  outputs = {
+    flake-parts,
+    nixcfg,
+    ...
+  } @ inputs: let
+    cfgLib = nixcfg.lib;
+  in
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = import inputs.systems;
+
+      imports = [
+        # Can be overridden with https://flake.parts/options/treefmt-nix options
+        nixcfg.flakeModules.treefmt
+      ];
+
+      flake = {
+        # Shared across all systems
+      };
+
+      perSystem = {pkgs, ...}: {
+        # Per system type
+        devShells = {
+          # Wrapper that sets the magic DEVSHELL variable, and preserves the user's default shell
+          # Usage: `nix develop [.#name=default]`
+          default = cfgLib.shell.mkShellEx pkgs.mkShellNoCC {
+            name = "dev";
+            packages = with pkgs; [
+              hello
+            ];
+
+            shellHook = ''
+              echo "Hello, world!"
+            '';
+          };
+        };
+
+        packages = {
+          # Usage: `nix run [.#name=default]`
+          default = pkgs.hello;
+        };
+      };
+    };
+}
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Template Repository
+
+This is a repository based on my
+[NixOS Configuration](https://github.com/kieranknowles1/nixcfg).
+
+See comments in [flake.nix](./flake.nix) for more information.
