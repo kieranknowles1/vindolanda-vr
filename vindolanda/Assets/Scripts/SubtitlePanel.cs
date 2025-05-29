ebd56f82aaf8e5f764ce6b2fb6f384a220f8ce2f
c4072047 <c4072047@18SCOMPD172>
2025-05-29 13:30:02 +0100

    game: fix subtitles being visible before any actor speaks

--- vindolanda/Assets/Scripts/SubtitlePanel.cs
+++ vindolanda/Assets/Scripts/SubtitlePanel.cs
@@ -6,6 +6,11 @@ public class SubtitlePanel : MonoBehaviour
     [SerializeField] Canvas canvas;
     [SerializeField] TextMeshProUGUI text;
 
+    private void Start()
+    {
+        Hide();
+    }
+
     public void Show(string str)
     {
         canvas.enabled = true;
