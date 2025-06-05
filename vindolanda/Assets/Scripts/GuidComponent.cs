76849811770685e822ae00ea925e7b30c34e2f0e
c4072047 <c4072047@18SCOMPD172>
2025-06-05 15:58:33 +0100

    game: fix build

--- vindolanda/Assets/Scripts/GuidComponent.cs
+++ vindolanda/Assets/Scripts/GuidComponent.cs
@@ -1,6 +1,7 @@
-using System;
+#if UNITY_EDITOR
 using UnityEditor;
 using UnityEditor.SceneManagement;
+#endif
 using UnityEngine;
 
 // Based on https://github.com/Unity-Technologies/guid-based-reference
