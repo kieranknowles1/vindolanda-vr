971ff000d897415362c1eda22ce044387793595e
c4072047 <c4072047@18SCOMPD172>
2025-06-09 15:16:35 +0100

    ai: add action to find object by ID
    used to avoid having to override or set it manually. Does however introduce a magic number in the object's GUID.

---
+++ vindolanda/Assets/Scripts/AI/FindGameObjectAction.cs
@@ -0,0 +1,20 @@
+using System;
+using Unity.Behavior;
+using UnityEngine;
+using Action = Unity.Behavior.Action;
+using Unity.Properties;
+
+[Serializable, GeneratePropertyBag]
+[NodeDescription(name: "FindGameObject", story: "Find [Object] with ID [Id]", category: "Action/Find", id: "fe719310804e1c1e0768520767eddfec")]
+public partial class FindGameObjectAction : Action
+{
+    [SerializeReference] public BlackboardVariable<GuidComponent> Object;
+    [SerializeReference] public BlackboardVariable<int> Id;
+
+    protected override Status OnStart()
+    {
+        Object.Value = (GuidComponent)GuidManager.Instance.TryFind(Id.Value);
+        return Object.Value != null ? Status.Success : Status.Failure;
+    }
+}
+
