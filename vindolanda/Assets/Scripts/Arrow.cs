b04a8b38a03e45260f1f4535426160e1f4453582
c4072047 <c4072047@18SCOMPD172>
2025-06-11 15:05:32 +0100

    bow: add basic arrow script

---
+++ vindolanda/Assets/Scripts/Arrow.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public interface IWeapon
+{
+
+}
+
+public interface IHitTarget
+{
+    void OnHit(IWeapon weapon);
+}
+
+[RequireComponent(typeof(Rigidbody))]
+public class Arrow : MonoBehaviour, IWeapon
+{
+    Rigidbody body;
+    SphereCollider trigger;
+
+    /// <summary>
+    /// Is the arrow stuck in an object and unable to move?
+    /// </summary>
+    public bool Stuck
+    {
+        get => !trigger.enabled;
+        set {
+            trigger.enabled = !value;
+            body.SetFrozen(value);
+        }
+    }
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+        trigger = GetComponent<SphereCollider>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var forwardVelocity = Vector3.Dot(body.linearVelocity, transform.forward);
+
+
+        var target = other.GetInterface<IHitTarget>();
+        target?.OnHit(this);
+
+        Stuck = true;
+    }
+}
---
+++ vindolanda/Assets/Scripts/ComponentExtensions.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public static class ComponentExtensions
+{
+    /// <summary>
+    /// Get the first <see cref="MonoBehaviour"/> that implements <typeparamref name="T"/>, or null on failure.
+    /// </summary>
+    public static T GetInterface<T>(this Component go) where T: class
+    {
+        foreach (var cmp in go.GetComponents<MonoBehaviour>())
+        {
+            if (cmp is T t) return t;
+        }
+        return null;
+    }
+
+    /// <summary>
+    /// Freeze the RigidBody in place, preventing all movement
+    /// </summary>
+    /// <param name="rb"></param>
+    public static void SetFrozen(this Rigidbody rb, bool state)
+    {
+        rb.constraints = state ? RigidbodyConstraints.FreezeAll : RigidbodyConstraints.None;
+    }
+}
