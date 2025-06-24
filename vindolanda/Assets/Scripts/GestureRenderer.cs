3aa23b57c1216fabc20e7c5c8e7b6bcca1cf7ec0
c4072047 <c4072047@18SCOMPD172>
2025-06-24 16:52:55 +0100

    gestures: support both smooth and instant transitions

--- vindolanda/Assets/Scripts/GestureRenderer.cs
+++ vindolanda/Assets/Scripts/GestureRenderer.cs
@@ -5,23 +5,41 @@ using UnityEngine;
 using UnityEngine.XR.Hands;
 using UnityEngine.XR.Hands.Gestures;
 
+/// <summary>
+/// Render a gesture to a hand rig. Makes the following assumptions:
+/// - Finger nodes follow the structure "root -> joint -> joint..."
+/// - All joints have exactly zero or one children
+///  - All joints in a finger rotate uniformly (not fully anatomically accurate)
+/// - Bones are never deleted at runtime
+/// - This component has exclusive ownership of joint rotations
+/// </summary>
 public class GestureRenderer : MonoBehaviour
 {
     [SerializeField] XRHandShape shape;
 
+    public XRHandShape Shape => shape;
+
     /// <summary>
-    /// On set, updates shape to match the requested pose.
-    /// Quite expensive, use sparingly
+    /// Instantly updates current shape to match the given pose. Moderately expensive, don't overuse
     /// </summary>
-    public XRHandShape Shape
+    /// <param name="shape"></param>
+    public void SetShapeInstant(XRHandShape shape)
     {
-        get => shape;
-        set
-        {
-            if (shape == value) return;
-            shape = value;
-            SmoothUpdatePose();
-        }
+        if (updateCoroutine != null) StopCoroutine(updateCoroutine);
+        this.shape = shape;
+        LerpPose(initialRotations, shape, 1.0f);
+    }
+
+    Coroutine updateCoroutine;
+    /// <summary>
+    /// Animate current shape to match the given pose. Quite expensive, use sparingly.
+    /// </summary>
+    /// <param name="shape"></param>
+    public void SetShapeSmooth(XRHandShape shape, float time)
+    {
+        this.shape = shape;
+        if (updateCoroutine != null) StopCoroutine(updateCoroutine);
+        updateCoroutine = StartCoroutine(UpdatePositions(shape, time));
     }
 
     [SerializeField] Transform pinky;
@@ -29,8 +47,6 @@ public class GestureRenderer : MonoBehaviour
     [SerializeField] Transform middle;
     [SerializeField] Transform index;
     [SerializeField] Transform thumb;
-
-    public float transitionTime = 1.0f;
 
     const float maxRotate = 40;
 
@@ -42,11 +58,12 @@ public class GestureRenderer : MonoBehaviour
         {
             static IEnumerable<Transform> Walk(Transform node)
             {
-                do
+                while (node.childCount > 0)
                 {
                     yield return node;
                     node = node.GetChild(0);
-                } while (node.childCount > 0);
+                }
+                yield return node; // Final joint
             }
             return Walk(pinky).Concat(Walk(ring)).Concat(Walk(middle)).Concat(Walk(index)).Concat(Walk(thumb));
         }
@@ -69,17 +86,32 @@ public class GestureRenderer : MonoBehaviour
     void Start()
     {
         initialRotations = GetCurrentRotations();
-        SmoothUpdatePose();
+        SetShapeInstant(shape);
     }
 
-    Coroutine updateCoroutine;
-    void SmoothUpdatePose()
+
+    /// <summary>
+    /// Interpolate to a pose
+    /// </summary>
+    /// <param name="from">Starting positions for nodes. Can be <see cref="initialRotations"/> for fully open or <see cref="GetCurrentRotations"/> for a different starting point.</param>
+    /// <param name="to">The final pose to reach</param>
+    /// <param name="ratio">Ratio to interpolate between</param>
+    void LerpPose(Dictionary<Transform, Quaternion> from, XRHandShape to, float ratio)
     {
-        if (updateCoroutine != null) StopCoroutine(updateCoroutine);
-        updateCoroutine = StartCoroutine(UpdatePositions());
+        foreach (var finger in to.fingerShapeConditions)
+        {
+            var node = GetFinger(finger.fingerID);
+            var rotate = Quaternion.Euler(Mathf.Lerp(0, maxRotate, finger.targets[0].desired), 0, 0);
+
+            while (node.childCount > 0)
+            {
+                node.localRotation = Quaternion.Lerp(from[node], initialRotations[node] * rotate, ratio);
+                node = node.GetChild(0);
+            }
+        }
     }
 
-    IEnumerator UpdatePositions()
+    IEnumerator UpdatePositions(XRHandShape to, float totalTime)
     {
         var starting = GetCurrentRotations();
         float time = 0;
@@ -87,30 +119,11 @@ public class GestureRenderer : MonoBehaviour
         do
         {
             time += Time.deltaTime;
-            float ratio = time / transitionTime;
+            float ratio = Mathf.Clamp01(time / totalTime);
             // Apply shape
-            foreach (var finger in shape.fingerShapeConditions)
-            {
-                var node = GetFinger(finger.fingerID);
-                var rotate = Quaternion.Euler(Mathf.Lerp(0, maxRotate, finger.targets[0].desired), 0, 0);
-
-                while (node.childCount > 0)
-                {
-                    node.localRotation = Quaternion.Lerp(starting[node], initialRotations[node] * rotate, ratio);
-                    node = node.GetChild(0);
-                }
-            }
+            LerpPose(starting, to, ratio);
 
             yield return new WaitForEndOfFrame();
-        } while (time < transitionTime);
-
-
-        // TODO: For testing
-        var tmp = open;
-        open = shape;
-        Shape = tmp;
+        } while (time < totalTime);
     }
-
-    // TODO: For testing
-    public XRHandShape open;
 }
