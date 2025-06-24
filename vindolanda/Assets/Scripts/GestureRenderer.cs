9a3e300978020aa2c0b66a60800820a7dac60541
c4072047 <c4072047@18SCOMPD172>
2025-06-24 16:34:07 +0100

    game: animate pose changes

--- vindolanda/Assets/Scripts/GestureRenderer.cs
+++ vindolanda/Assets/Scripts/GestureRenderer.cs
@@ -1,3 +1,6 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.XR.Hands;
 using UnityEngine.XR.Hands.Gestures;
@@ -6,13 +9,48 @@ public class GestureRenderer : MonoBehaviour
 {
     [SerializeField] XRHandShape shape;
 
+    /// <summary>
+    /// On set, updates shape to match the requested pose.
+    /// Quite expensive, use sparingly
+    /// </summary>
+    public XRHandShape Shape
+    {
+        get => shape;
+        set
+        {
+            if (shape == value) return;
+            shape = value;
+            SmoothUpdatePose();
+        }
+    }
+
     [SerializeField] Transform pinky;
     [SerializeField] Transform ring;
     [SerializeField] Transform middle;
     [SerializeField] Transform index;
     [SerializeField] Transform thumb;
 
+    public float transitionTime = 1.0f;
+
     const float maxRotate = 40;
+
+    Dictionary<Transform, Quaternion> GetCurrentRotations() => AllFingerNodes.ToDictionary(n => n, n => n.localRotation);
+    Dictionary<Transform, Quaternion> initialRotations;
+
+    IEnumerable<Transform> AllFingerNodes {
+        get
+        {
+            static IEnumerable<Transform> Walk(Transform node)
+            {
+                do
+                {
+                    yield return node;
+                    node = node.GetChild(0);
+                } while (node.childCount > 0);
+            }
+            return Walk(pinky).Concat(Walk(ring)).Concat(Walk(middle)).Concat(Walk(index)).Concat(Walk(thumb));
+        }
+    }
 
     Transform GetFinger(XRHandFingerID finger)
     {
@@ -30,18 +68,49 @@ public class GestureRenderer : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        if (shape == null) return;
-
-        foreach (var finger in shape.fingerShapeConditions)
-        {
-            var node = GetFinger(finger.fingerID);
-            var rotate = Quaternion.Euler(Mathf.Lerp(0, maxRotate, finger.targets[0].desired), 0, 0);
-
-            do
-            {
-                node.rotation *= rotate;
-                node = node.GetChild(0);
-            } while (node.childCount > 0);
-        }
+        initialRotations = GetCurrentRotations();
+        SmoothUpdatePose();
     }
+
+    Coroutine updateCoroutine;
+    void SmoothUpdatePose()
+    {
+        if (updateCoroutine != null) StopCoroutine(updateCoroutine);
+        updateCoroutine = StartCoroutine(UpdatePositions());
+    }
+
+    IEnumerator UpdatePositions()
+    {
+        var starting = GetCurrentRotations();
+        float time = 0;
+
+        do
+        {
+            time += Time.deltaTime;
+            float ratio = time / transitionTime;
+            // Apply shape
+            foreach (var finger in shape.fingerShapeConditions)
+            {
+                var node = GetFinger(finger.fingerID);
+                var rotate = Quaternion.Euler(Mathf.Lerp(0, maxRotate, finger.targets[0].desired), 0, 0);
+
+                while (node.childCount > 0)
+                {
+                    node.localRotation = Quaternion.Lerp(starting[node], initialRotations[node] * rotate, ratio);
+                    node = node.GetChild(0);
+                }
+            }
+
+            yield return new WaitForEndOfFrame();
+        } while (time < transitionTime);
+
+
+        // TODO: For testing
+        var tmp = open;
+        open = shape;
+        Shape = tmp;
+    }
+
+    // TODO: For testing
+    public XRHandShape open;
 }
