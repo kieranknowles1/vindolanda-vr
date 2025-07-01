035f109188a2a9cb31465fe44c876ac06bf6deb1
c4072047 <c4072047@18SCOMPD172>
2025-07-01 13:13:08 +0100

    game: add sit/stand functionality

--- vindolanda/Assets/Scripts/Furniture.cs
+++ vindolanda/Assets/Scripts/Furniture.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -7,39 +8,43 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class Furniture : MonoBehaviour
 {
+    public static string SitAnimName = "SittingIdle";
+    public static string StandToSitAnimName = "StandToSit";
+    public static int SitVariableId = Animator.StringToHash("Sit");
+
     public AnimatorOverrideController sitOverrides;
-    public GameObject entryPoint;
+    public Transform entryPoint;
+
+    AnimationClip GetAnimation(string name)
+    {
+        var overrides = new List<KeyValuePair<AnimationClip, AnimationClip>>();
+        sitOverrides.GetOverrides(overrides);
+        return overrides.First(o => o.Key.name == name).Value;
+    }
 
 #if UNITY_EDITOR
     [SerializeField] GameObject preview;
     GameObject previewInstance = null;
-    GameObject previewInstanceStand = null;
 
-    void DrawPreview(ref GameObject instance, string animation)
+    void DrawPreview(ref GameObject instance, string animation, Transform parent)
     {
         if (instance == null)
         {
             instance = Instantiate(preview);
             instance.hideFlags = HideFlags.HideAndDontSave;
-            instance.transform.parent = transform;
-            instance.transform.SetPositionAndRotation(transform.position, transform.rotation);
+            instance.transform.parent = parent;
+            instance.transform.SetPositionAndRotation(parent.position, parent.rotation);
         }
 
-        var overrides = new List<KeyValuePair<AnimationClip, AnimationClip>>();
-        sitOverrides.GetOverrides(overrides);
-        var sit = overrides.First(o => o.Key.name == animation).Value;
-        sit.SampleAnimation(instance, 0);
-
-        // Apply root motion from sit -> stand for its preview
-        instance.transform.position = transform.position - (sit.averageSpeed * sit.averageDuration);
+        var sit = GetAnimation(animation);
+        sit.SampleAnimation(instance, sit.averageDuration);
     }
 
     private void OnDrawGizmosSelected()
     {
         if (sitOverrides == null || preview == null) return;
 
-        DrawPreview(ref previewInstance, "SittingIdle");
-        DrawPreview(ref previewInstanceStand, "StandToSit");
+        DrawPreview(ref previewInstance, StandToSitAnimName, entryPoint);
     }
 
     private void OnEnable()
@@ -47,17 +52,8 @@ public class Furniture : MonoBehaviour
         if (sitOverrides == null) return;
         if (preview == null) return;
 
+        DrawPreview(ref previewInstance, StandToSitAnimName, entryPoint);
         previewInstance.SetActive(true);
-        DrawPreview(ref previewInstance, "SittingIdle");
-        DrawPreview(ref previewInstanceStand, "StandToSit");
-
-        if (entryPoint != null)
-        {
-            if (entryPoint.transform.localPosition != previewInstanceStand.transform.localPosition)
-            {
-                entryPoint.transform.localPosition = previewInstanceStand.transform.localPosition;
-            }
-        }
     }
 
     private void OnDisable()
@@ -67,5 +63,72 @@ public class Furniture : MonoBehaviour
             previewInstance.SetActive(false);
         }
     }
+
+    private void Start()
+    {
+        if (previewInstance != null) previewInstance.SetActive(false);
+    }
 #endif
+
+    float sitDuration = float.NegativeInfinity;
+    /// <summary>
+    /// How long does it take to go from standing to sitting
+    /// </summary>
+    public float SitDuration
+    {
+        get
+        {
+            if (sitDuration == float.NegativeInfinity)
+            {
+                var sit = GetAnimation("StandToSit");
+                sitDuration = sit.averageDuration;
+            }
+            return sitDuration;
+        }
+    }
+
+    public ActorController CurrentActor { get; private set; }
+    RuntimeAnimatorController animationBak;
+    public IEnumerator Sit(ActorController actor)
+    {
+        if (CurrentActor != null)
+        {
+            throw new System.Exception($"Furniture already in use by {CurrentActor.name}");
+        }
+        CurrentActor = actor;
+        // Position actor to enter smoothly
+        actor.transform.SetPositionAndRotation(entryPoint.transform.position, entryPoint.transform.rotation);
+
+        animationBak = actor.animator.runtimeAnimatorController;
+        actor.animator.runtimeAnimatorController = sitOverrides;
+        actor.animator.SetBool(SitVariableId, true);
+
+        yield return LerpPosition(actor, entryPoint, transform);
+    }
+
+    // HACK: Enabling root motion causes sliding during the transition from SitToStand -> Sit
+    IEnumerator LerpPosition(ActorController actor, Transform start, Transform end)
+    {
+        float time = 0;
+        while (time < SitDuration)
+        {
+            time += Time.deltaTime;
+            float ratio = time / SitDuration;
+            actor.transform.position = Vector3.Lerp(start.position, end.position, ratio);
+            yield return new WaitForFixedUpdate();
+        }
+    }
+
+    public IEnumerator Stand(ActorController actor)
+    {
+        if (CurrentActor != actor)
+        {
+            throw new System.Exception($"{actor.name} tried to stand from furniture it is not sitting");
+        }
+        actor.animator.SetBool(SitVariableId, false);
+        yield return LerpPosition(actor, transform, entryPoint);
+        actor.animator.runtimeAnimatorController = animationBak;
+        animationBak = null;
+        CurrentActor = null;
+    }
 }
