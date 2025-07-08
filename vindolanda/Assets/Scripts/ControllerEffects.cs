b5c909e97b07a962fb7b7ae7b270ecf7efdfd3d2
c4072047 <c4072047@18SCOMPD172>
2025-07-08 15:58:55 +0100

    game: add glow for controllers

---
+++ vindolanda/Assets/Scripts/ControllerEffects.cs
@@ -0,0 +1,54 @@
+using System;
+using UnityEngine;
+
+public class ControllerEffects : MonoBehaviour
+{
+    [Flags]
+    public enum ControllerButton
+    {
+        None = 0,
+        Grip = 1 << 0,
+        Trigger = 1 << 1,
+        Stick = 1 << 2,
+        A = 1 << 3,
+        B = 1 << 4,
+    }
+
+    public Material glow;
+    Material originalMaterial;
+
+    public MeshRenderer grip;
+    public MeshRenderer trigger;
+    public MeshRenderer stick;
+    public MeshRenderer buttonA;
+    public MeshRenderer buttonB;
+
+    ControllerButton glowState;
+    public ControllerButton GlowState
+    {
+        get => glowState;
+        set
+        {
+            glowState = value;
+            UpdateGlow(value, grip, ControllerButton.Grip);
+            UpdateGlow(value, trigger, ControllerButton.Trigger);
+            UpdateGlow(value, stick, ControllerButton.Stick);
+            UpdateGlow(value, buttonA, ControllerButton.A);
+            UpdateGlow(value, buttonB, ControllerButton.B);
+        }
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        originalMaterial = grip.sharedMaterial;
+
+        GlowState |= ControllerButton.A | ControllerButton.B;
+    }
+
+    void UpdateGlow(ControllerButton state, MeshRenderer button, ControllerButton mask)
+    {
+        bool lit = state.HasFlag(mask);
+        button.sharedMaterial = lit ? glow : originalMaterial;
+    }
+}
