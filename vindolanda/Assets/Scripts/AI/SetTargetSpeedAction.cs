c03d157ef5c70c0add9d09e1c60282ba0fa2d0df
c4072047 <c4072047@18SCOMPD172>
2025-07-08 13:15:52 +0100

    game: smoothly update actor speed over time

--- vindolanda/Assets/Scripts/AI/SetTargetSpeedAction.cs
+++ vindolanda/Assets/Scripts/AI/SetTargetSpeedAction.cs
@@ -3,14 +3,14 @@ using Unity.Behavior;
 using UnityEngine;
 using Action = Unity.Behavior.Action;
 using Unity.Properties;
+using UnityEngine.AI;
 
 [Serializable, GeneratePropertyBag]
-[NodeDescription(name: "SetTargetSpeed", story: "Set [Output] based on [Agent] distance to [Target]", category: "Action", id: "c6d3c85249d5d6d40edca16a5130d086")]
+[NodeDescription(name: "SetTargetSpeed", story: "Set [Agent] speed based on distance to [Target]", category: "Action", id: "c6d3c85249d5d6d40edca16a5130d086")]
 public partial class SetTargetSpeedAction : Action
 {
     [SerializeReference] public BlackboardVariable<GameObject> Agent;
     [SerializeReference] public BlackboardVariable<GameObject> Target;
-    [SerializeReference] public BlackboardVariable<float> Output;
 
     [Tooltip("When at this distance or below, walk")]
     [SerializeReference] public BlackboardVariable<float> WalkAtDistance = new(7.0f);
@@ -19,12 +19,42 @@ public partial class SetTargetSpeedAction : Action
     [SerializeReference] public BlackboardVariable<float> WalkSpeed = new(1.0f);
     [SerializeReference] public BlackboardVariable<float> RunSpeed = new(4.0f);
 
+    [Tooltip("Duration to smooth speed update over. If zero, run instantly")]
+    [SerializeReference] public BlackboardVariable<float> TransitionDuration = new(0.5f);
+
+    NavMeshAgent navAgent;
+    float startSpeed;
+    float endSpeed;
+    float elapsed;
+
     protected override Status OnStart()
     {
-        float distance = Agent.Value.transform.GetDistance(Target.Value.transform);
+        navAgent = Agent.Value.GetComponent<NavMeshAgent>();
+        if (navAgent == null) return Status.Failure;
 
-        Output.Value = CalculateTargetSpeed(distance);
-        return Status.Success;
+        float distance = Agent.Value.transform.GetDistance(Target.Value.transform);
+        startSpeed = navAgent.speed;
+        endSpeed = CalculateTargetSpeed(distance);
+        elapsed = 0;
+        Debug.Log(startSpeed + " " + endSpeed);
+
+        if (TransitionDuration.Value == 0)
+        {
+            OnUpdate();
+            return Status.Running;
+        }
+        else
+        {
+            return Status.Running;
+        }
+    }
+
+    protected override Status OnUpdate()
+    {
+        elapsed += Time.deltaTime;
+        float ratio = TransitionDuration.Value > 0 ? elapsed / TransitionDuration.Value : 1.0f;
+        navAgent.speed = Mathf.Lerp(startSpeed, endSpeed, ratio);
+        return elapsed >= TransitionDuration.Value ? Status.Success : Status.Running;
     }
 
     float CalculateTargetSpeed(float distance)
