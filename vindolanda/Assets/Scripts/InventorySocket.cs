20bdd15d2ef4688d71e0c9c75ebca7d5e39d8132
Kieran <kieranknowles11@hotmail.co.uk>
2025-07-31 09:46:44 +0100

    game: add slot for writing tablet

--- vindolanda/Assets/Scripts/InventorySocket.cs
+++ vindolanda/Assets/Scripts/InventorySocket.cs
@@ -5,13 +5,31 @@ using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
 public class InventorySocket : XRSocketInteractor
 {
+    public enum InventoryItem
+    {
+        Any,
+        WritingTablet
+    };
+
     [Tooltip("If set, only pick up this item.")]
     public XRGrabInteractable itemFilter;
+
+    [Tooltip("If set, only pick up this item type.")]
+    public InventoryItem typeFilter;
 
     [Tooltip("If set, snap items back after dropping.")]
     public bool snapReturn;
 
-    bool IsTarget(IXRInteractable interactable) => itemFilter == null || (object)interactable == itemFilter;
+    bool IsTarget(IXRInteractable interactable) {
+        if (itemFilter != null && (object)interactable == itemFilter) return true;
+
+        return typeFilter switch
+        {
+            InventoryItem.Any => true,
+            InventoryItem.WritingTablet => interactable is WritingTablet,
+            _ => throw new UnreachableException()
+        };
+    }
 
     public override bool CanSelect(IXRSelectInteractable interactable)
     {
@@ -29,6 +47,7 @@ public class InventorySocket : XRSocketInteractor
         // Don't tie movement to the player
         args.interactableObject.transform.parent = null;
 
+        // Force item back into us when dropped
         if (snapReturn)
         {
             args.interactableObject.selectExited.AddListener(SnapBack);
